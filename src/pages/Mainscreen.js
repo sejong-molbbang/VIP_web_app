7898f7976f7f7c43bd21e4bfafca03c0ded26179
yeongjoon <go1217jo@naver.com>
2019-06-02 00:20:12 +0900

    mainscreen 클래스화

--- src/pages/Mainscreen.js
+++ src/pages/Mainscreen.js
@@ -1,63 +1,68 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
-import Login from 'components/Login';
 import {Container, Divider, Dropdown, Grid, Header, Image,  List, Menu, Segment,} from 'semantic-ui-react'
 import logo from 'image/logo.png';
 import ReactPlayer from 'react-player'
-import FileUpload from '../upload/upload.js' ;
-import ImageModal from '../modal/imagemodal.js';
-import VideoModal from '../modal/videomodal.js';
+import FileUpload from 'upload/upload.js' ;
+import ImageModal from 'modal/imagemodal.js';
+import VideoModal from 'modal/videomodal.js';
 
 
-
-const FixedMenuLayout = () => (
-  <div>
-    <Menu fixed='top' inverted>
-      <Container>
-        <Menu.Item as='a' header >
-          <Image size='mini' src={logo} style={{ marginRight: '1.5em' }} />
-          VIP WEB
-        </Menu.Item>
-        <Menu.Item as='a'>Personal Infomation Protect</Menu.Item>
-        <button class="negative ui button" style={{ marginLeft: '63.6em' }}>Logout</button>
-      </Container>
-    </Menu>
-
-    <Container text style={{ marginTop: '5em'}}>
-
-     <div>
-           <ReactPlayer text style={{ marginTop: '7em'}}  url='https://www.youtube.com/watch?v=LCKwD1xLLNk' playing />  
+class Mainscreen extends Component {
+    state = {
+      logout : false,
+    }
+    render() {
+        return (
+            <div>
+              <Menu fixed='top' inverted>
+                <Container>
+                  <Menu.Item as='a' header >
+                    <Image size='mini' src={logo} style={{ marginRight: '1.5em' }} />
+                    VIP WEB
+                  </Menu.Item>
+                  <Menu.Item as='a'>Personal Infomation Protect</Menu.Item>
+                  <button class="negative ui button" style={{ marginLeft: '63.6em' }}>Logout</button>
+                </Container>
+              </Menu>
           
-           <div class="ui toggle checkbox" style={{ marginTop: '3em'}}>
-                <input type="checkbox" name="public"/>
-                <label >얼굴만 인식</label>
-           </div>
-
-           
+              <Container text style={{ marginTop: '5em'}}>
           
-           <div class="ui four column doubling stackable grid container" style={{ marginTop: '2em'}}>
+               <div>
+                     <ReactPlayer text style={{ marginTop: '7em'}}  url='https://www.youtube.com/watch?v=LCKwD1xLLNk' playing />  
+                    
+                     <div class="ui toggle checkbox" style={{ marginTop: '3em'}}>
+                          <input type="checkbox" name="public"/>
+                          <label >얼굴만 인식</label>
+                     </div>
+          
+                     
+                    
+                     <div class="ui four column doubling stackable grid container" style={{ marginTop: '2em'}}>
+          
+                       <div class="column">
+                          <VideoModal></VideoModal>
+                       </div>
+          
+                       <div class="column">
+                          <ImageModal></ImageModal>
+                       </div>
+          
+                       <div class="column">
+                          <button class="ui inverted green button" style={{ marginLeft: '-1.5em' }} >수작업 마스킹</button>
+                       </div>
+          
+                       <div class="column">
+                          <button class="ui inverted red button" style={{ marginLeft: '-1.5em' }} >파일변환 시작</button>
+                       </div>           
+          
+                      </div>  
+               </div>
+              </Container>   
+            </div>
+        )
+    }
+}
 
-             <div class="column">
-                <VideoModal></VideoModal>
-             </div>
-
-             <div class="column">
-                <ImageModal></ImageModal>
-             </div>
-
-             <div class="column">
-                <button class="ui inverted green button" style={{ marginLeft: '-1.5em' }} >수작업 마스킹</button>
-             </div>
-
-             <div class="column">
-                <button class="ui inverted red button" style={{ marginLeft: '-1.5em' }} >파일변환 시작</button>
-             </div>           
-
-            </div>  
-     </div>
-    </Container>   
-  </div>
-)
-
-export default FixedMenuLayout
+export default Mainscreen
 
